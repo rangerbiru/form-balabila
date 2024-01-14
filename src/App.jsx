16128831d1a2efe0753582884cd84cdcc7a8976c
rangerbiru <rangerbiru.id@gmail.com>
2024-01-14 14:31:29 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FormInput from "./FormInput";
+import Tes from "./Tes";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/form-bikers/">
         <Routes>
           <Route path="/" element={<FormInput />} />
+          <Route path="/tes" element={<Tes />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Tes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tes = () => {
+  return (
+    <div>
+      <h2>Testing</h2>
+    </div>
+  );
+};
+
+export default Tes;
